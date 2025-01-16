b03ceefd1bb9bc44c831f50caf777e8a7eb6710f
Il Harper <hi@ilharper.com>
2025-01-17 01:53:04 +0800

    feat(components): add styles

--- packages/components/.storybook/preview.ts
+++ packages/components/.storybook/preview.ts
@@ -1,5 +1,9 @@
 import type { Preview } from '@storybook/react'
 
+import 'normalize.css'
+import '../src/styles/layout/index.scss'
+import '../src/styles/theme-sym/index.scss'
+
 const preview: Preview = {
   parameters: {
     controls: {
