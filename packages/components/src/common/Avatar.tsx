8fbee1bb6ead2e9fb45c4e66e9a8d99e605f19db
Il Harper <hi@ilharper.com>
2025-01-26 15:47:35 +0800

    feat(component): add `Avatar`

---
+++ packages/components/src/common/Avatar.tsx
@@ -0,0 +1,47 @@
+'use client'
+
+import * as AvatarPrimitive from '@radix-ui/react-avatar'
+import type { ComponentPropsWithoutRef, ComponentRef } from 'react'
+import { forwardRef } from 'react'
+import { clsx } from 'clsx'
+
+const Avatar = forwardRef<
+  ComponentRef<typeof AvatarPrimitive.Root>,
+  ComponentPropsWithoutRef<typeof AvatarPrimitive.Root>
+>(({ className, ...props }, ref) => (
+  <AvatarPrimitive.Root
+    ref={ref}
+    className={clsx('sym-avatar', className)}
+    {...props}
+  />
+))
+
+Avatar.displayName = AvatarPrimitive.Root.displayName
+
+const AvatarImage = forwardRef<
+  ComponentRef<typeof AvatarPrimitive.Image>,
+  ComponentPropsWithoutRef<typeof AvatarPrimitive.Image>
+>(({ className, ...props }, ref) => (
+  <AvatarPrimitive.Image
+    ref={ref}
+    className={clsx('sym-avatar-image', className)}
+    {...props}
+  />
+))
+
+AvatarImage.displayName = AvatarPrimitive.Image.displayName
+
+const AvatarFallback = forwardRef<
+  ComponentRef<typeof AvatarPrimitive.Fallback>,
+  ComponentPropsWithoutRef<typeof AvatarPrimitive.Fallback>
+>(({ className, ...props }, ref) => (
+  <AvatarPrimitive.Fallback
+    ref={ref}
+    className={clsx('sym-avatar-fallback', className)}
+    {...props}
+  />
+))
+
+AvatarFallback.displayName = AvatarPrimitive.Fallback.displayName
+
+export { Avatar, AvatarFallback, AvatarImage }
