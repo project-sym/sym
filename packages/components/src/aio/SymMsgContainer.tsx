43498dfc29ec944a611c93c329ae32b4955e700b
Il Harper <hi@ilharper.com>
2025-01-26 21:20:40 +0800

    feat(components): update `SymMsgContainer`

--- packages/components/src/aio/SymMsgContainer.tsx
+++ packages/components/src/aio/SymMsgContainer.tsx
@@ -14,7 +14,9 @@ export const SymMsgContainer = ({ children }: SymMsgContainerProps) => {
       <div className="sym-aio-msg-gutter" />
       <SymAioAvatarContainer />
       <div className="sym-aio-msg-body-container">
-        <div className="sym-aio-msg-header-container">{msg.symHeader}</div>
+        {msg.symHeader && (
+          <div className="sym-aio-msg-header-container">{msg.symHeader}</div>
+        )}
         <div className="sym-aio-msg-content-container">{children}</div>
       </div>
     </div>
