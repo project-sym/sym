d4a63d90a84958230487c810db14629d2933e265
Il Harper <hi@ilharper.com>
2025-01-26 21:37:26 +0800

    feat(components): `SymMsgContainer` adapt `SymMsgGroupContext`

--- packages/components/src/aio/SymMsgContainer.tsx
+++ packages/components/src/aio/SymMsgContainer.tsx
@@ -1,18 +1,24 @@
 import type { ReactNode } from 'react'
 import { SymAioAvatarContainer } from './SymAioAvatarContainer'
 import { useSymAioMsg } from './aioMsg'
+import { useSymMsgGroup } from './SymMsgGroup'
 
 export interface SymMsgContainerProps {
   children: ReactNode
 }
 
 export const SymMsgContainer = ({ children }: SymMsgContainerProps) => {
+  const msgGroup = useSymMsgGroup()
   const msg = useSymAioMsg()
 
   return (
     <div className="sym-aio-msg-container">
       <div className="sym-aio-msg-gutter" />
-      <SymAioAvatarContainer />
+      {msgGroup.group ? (
+        <div className="sym-aio-avatar-placeholder" />
+      ) : (
+        <SymAioAvatarContainer />
+      )}
       <div className="sym-aio-msg-body-container">
         {msg.symHeader && (
           <div className="sym-aio-msg-header-container">{msg.symHeader}</div>
