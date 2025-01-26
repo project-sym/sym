e6ce4697f4f83b7d24b1b6195e9a17be7cf7e7dd
Il Harper <hi@ilharper.com>
2025-01-26 20:55:51 +0800

    feat(components): implement msg header

--- packages/components/src/aio/SymMsgContainer.tsx
+++ packages/components/src/aio/SymMsgContainer.tsx
@@ -1,17 +1,20 @@
 import type { ReactNode } from 'react'
 import { SymAioAvatarContainer } from './SymAioAvatarContainer'
+import { useSymAioMsg } from './aioMsg'
 
 export interface SymMsgContainerProps {
   children: ReactNode
 }
 
 export const SymMsgContainer = ({ children }: SymMsgContainerProps) => {
+  const msg = useSymAioMsg()
+
   return (
     <div className="sym-aio-msg-container">
       <div className="sym-aio-msg-gutter" />
       <SymAioAvatarContainer />
       <div className="sym-aio-msg-body-container">
-        <div className="sym-aio-msg-header-container"></div>
+        <div className="sym-aio-msg-header-container">{msg.symHeader}</div>
         <div className="sym-aio-msg-content-container">{children}</div>
       </div>
     </div>
--- packages/components/src/aio/aioMsg.ts
+++ packages/components/src/aio/aioMsg.ts
@@ -1,7 +1,10 @@
 import type { Message } from '@satorijs/protocol'
+import type { ReactNode } from 'react'
 import { createContext, useContext } from 'react'
 
-export interface SymAioMsg extends Omit<Message, 'messageId'> {}
+export interface SymAioMsg extends Omit<Message, 'messageId'> {
+  symHeader?: ReactNode | null | undefined
+}
 
 export const SymMsgContext = createContext<SymAioMsg>(
   undefined as unknown as SymAioMsg,
