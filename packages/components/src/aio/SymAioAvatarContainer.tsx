270cbf3d96528c93e389120069564d797f0c70b4
Il Harper <hi@ilharper.com>
2025-01-26 20:35:15 +0800

    feat(components): update `SymAioAvatarContainer`

--- packages/components/src/aio/SymAioAvatarContainer.tsx
+++ packages/components/src/aio/SymAioAvatarContainer.tsx
@@ -1,3 +1,9 @@
+import { Avatar, AvatarFallback } from '../common/Avatar'
+
 export const SymAioAvatarContainer = () => (
-  <div className="sym-aio-avatar-container" />
+  <div className="sym-aio-avatar-container">
+    <Avatar className="sym-aio-avatar">
+      <AvatarFallback>CN</AvatarFallback>
+    </Avatar>
+  </div>
 )
