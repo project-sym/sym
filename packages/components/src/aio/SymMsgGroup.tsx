8d145c7479fb8eb00b469fffc3aff410f81d2d0d
Il Harper <hi@ilharper.com>
2025-01-26 22:56:39 +0800

    feat(components): implement `SymMsgGroup`

--- packages/components/src/aio/SymMsgGroup.tsx
+++ packages/components/src/aio/SymMsgGroup.tsx
@@ -1,19 +1,39 @@
 import { createContext, useContext } from 'react'
+import { useSymAioCtx } from './SymAio'
+import { SymAioAvatarContainer } from './SymAioAvatarContainer'
+import { SymMsg } from './SymMsg'
 
 export interface SymMsgGroupCtx {
-  group: boolean
+  group?: boolean | undefined
 }
 
 export const SymMsgGroupContext = createContext<SymMsgGroupCtx>({
   group: true,
 })
 
-export const useSymMsgGroup = () => {
-  const context = useContext(SymMsgGroupContext)
+export const useSymMsgGroup = () => useContext(SymMsgGroupContext)
 
-  return {
-    ...context,
-  }
+export const SymMsgGroup = () => {
+  const aioCtx = useSymAioCtx()
+
+  return (
+    <div className="sym-aio-msg-group-container">
+      <div className="sym-aio-msg-group-content">
+        {aioCtx.messages[0] && <SymMsg msg={aioCtx.messages[0]} />}
+        {aioCtx.messages[1] &&
+          aioCtx.messages.slice(1).map((x) => (
+            <SymMsg
+              msg={{
+                ...x,
+                symHeader: undefined,
+              }}
+            />
+          ))}
+      </div>
+      <div className="sym-aio-msg-group-overlay-container">
+        <div className="sym-aio-msg-gutter-container" />
+        <SymAioAvatarContainer />
+      </div>
+    </div>
+  )
 }
-
-export const SymMsgGroup = () => <></>
