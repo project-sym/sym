677637e27fce98e55cd717e57acce8d29b1e73a5
Il Harper <hi@ilharper.com>
2025-01-30 01:36:28 +0800

    feat(components): implement basic `SymMsgVirtualList`

--- packages/components/src/aio/SymMsgVirtualList.tsx
+++ packages/components/src/aio/SymMsgVirtualList.tsx
@@ -1,1 +1,14 @@
-export const SymMsgVirtualList = () => <></>
+import { useSymAioCtx } from './SymAio'
+import { SymMsgGroup } from './SymMsgGroup'
+
+export const SymMsgVirtualList = () => {
+  const aioCtx = useSymAioCtx()
+
+  return (
+    <div className="sym-aio-msg-list-container">
+      {aioCtx.messages.map((x) => (
+        <SymMsgGroup msgs={[x]} />
+      ))}
+    </div>
+  )
+}
