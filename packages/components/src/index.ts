a78a521e24935c7de708ccb3b4784d9dfa0d3407
Il Harper <hi@ilharper.com>
2025-04-13 16:16:37 +0800

    feat(components): add build scripts

---
+++ packages/components/src/index.ts
@@ -0,0 +1,6 @@
+import '../src/styles/layout/index.scss'
+import '../src/styles/theme-sym/index.scss'
+
+export * from './aio'
+export * from './common'
+export * from './host'
--- packages/components/vite.config.ts
+++ packages/components/vite.config.ts
@@ -1,6 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'node:path'
+import dts from 'vite-plugin-dts'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({
+      rollupTypes: true,
+      tsconfigPath: './tsconfig.app.json',
+    }),
+  ],
+
+  build: {
+    lib: {
+      entry: resolve(import.meta.dirname, 'src/index.ts'),
+      fileName: 'index',
+      formats: ['es'],
+    },
+
+    rollupOptions: {
+      external: ['react', 'react/jsx-runtime', 'react-dom'],
+    },
+  },
 })
