da2f9e6b89604ec8259c11a980b94e42cf7ca649
Il Harper <hi@ilharper.com>
2025-04-13 16:19:59 +0800

    feat(components): update build scripts

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -1,6 +1,3 @@
-import '../src/styles/layout/index.scss'
-import '../src/styles/theme-sym/index.scss'
-
 export * from './aio'
 export * from './common'
 export * from './host'
--- packages/components/vite.config.ts
+++ packages/components/vite.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
     },
 
     rollupOptions: {
-      external: ['react', 'react/jsx-runtime', 'react-dom'],
+      external: ['react', 'react/jsx-runtime'],
     },
   },
 })
