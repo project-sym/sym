7f9933b995a32877b4fa6f06a7006800aef211ff
Il Harper <hi@ilharper.com>
2025-06-16 03:44:31 +0800

    feat(components): implement `SymMsgVirtualList`

--- packages/components/src/aio/SymMsgVirtualList.tsx
+++ packages/components/src/aio/SymMsgVirtualList.tsx
@@ -1,3 +1,4 @@
+import type { SymAioMsg } from './aioMsg'
 import { useSymAioCtx } from './SymAio'
 import { SymMsgGroup } from './SymMsgGroup'
 
@@ -10,9 +11,27 @@ export const SymMsgVirtualList = ({ className }: SymMsgVirtualListProps) => {
 
   return (
     <div className={`sym-aio-msg-list-container ${className}`}>
-      {aioCtx.messages.map((x) => (
-        <SymMsgGroup msgs={[x]} />
-      ))}
+      {aioCtx.messages
+        .reduce<SymAioMsg[][]>((r, c) => {
+          if (!r.length) return [[c]]
+
+          const prevArr = r[r.length - 1]!
+          const prevMsg = prevArr[prevArr.length - 1]!
+          const prevId = prevMsg.user?.id
+
+          const curId = c.user?.id
+
+          if (!prevId || !curId || prevId !== curId) {
+            r.push([c])
+          } else {
+            prevArr.push(c)
+          }
+
+          return r
+        }, [])
+        .map((x) => (
+          <SymMsgGroup msgs={x} />
+        ))}
     </div>
   )
 }
